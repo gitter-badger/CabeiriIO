4179778bafddc171fbd8b8b21bc7937dfe862a3a
Vincent Picard <vincentpcrd@gmail.com>
2016-04-27 16:24:49 -0400

    CID now properly generates unique ids
    good enough for getting unique ids accross multiple clients. There could be collisions (very low probability).

--- cabeiri-common/ts/src/native.generator/fundamentals/cid.js
+++ cabeiri-common/ts/src/native.generator/fundamentals/cid.js
@@ -16,8 +16,27 @@ System.register([], function(exports_1, context_1) {
                 CID.prototype.getID = function () {
                     return this.id + "";
                 };
+                /**
+                 * Generates a new unique CID.
+                 */
                 CID.GetNewCID = function () {
-                    return CID_NONE; // :(
+                    return new CID(CID.generateUUID());
+                };
+                /**
+                 * Generates unique id using date and random.
+                 * very low probability of having collision, still we should pay attention to that when uploading id on server.
+                 */
+                CID.generateUUID = function () {
+                    var date = new Date().getTime();
+                    if (window.performance && typeof window.performance.now === "function") {
+                        date += performance.now(); //use high-precision timer if available
+                    }
+                    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+                        var random = (date + Math.random() * 16) % 16 | 0;
+                        date = Math.floor(date / 16);
+                        return (c == 'x' ? random : (random & 0x3 | 0x8)).toString(16);
+                    });
+                    return uuid;
                 };
                 return CID;
             }());
@@ -25,7 +44,7 @@ System.register([], function(exports_1, context_1) {
             /**
              * Kind of a null pointer :)
              */
-            exports_1("CID_NONE", CID_NONE = new CID(-1));
+            exports_1("CID_NONE", CID_NONE = new CID("ID_NONE"));
         }
     }
 });
--- cabeiri-common/ts/src/native.generator/fundamentals/cid.ts
+++ cabeiri-common/ts/src/native.generator/fundamentals/cid.ts
@@ -3,20 +3,44 @@
  */
 export class CID 
 {
+    
     //for now, using a number...
-    constructor (public id : number) {}
+    constructor (public id : string) {}
     public getID() : string
     {
         return this.id + "";
     }
     
+    /**
+     * Generates a new unique CID.
+     */
     public static GetNewCID() : CID
     {
-        return CID_NONE; // :(
+        return new CID(CID.generateUUID());
+    }
+    
+    /**
+     * Generates unique id using date and random. 
+     * very low probability of having collision, still we should pay attention to that when uploading id on server.
+     */
+    private static generateUUID() : string 
+    {
+        var date : number = new Date().getTime();
+        if(window.performance && typeof window.performance.now === "function")
+        {
+            date += performance.now(); //use high-precision timer if available
+        }
+        var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) 
+        {
+            var random = (date + Math.random()*16)%16 | 0;
+            date = Math.floor(date/16);
+            return (c=='x' ? random : (random&0x3|0x8)).toString(16);
+        });
+        return uuid;
     }
 }
 /**
  * Kind of a null pointer :)
  */   
-export const CID_NONE : CID = new CID(-1);
+export const CID_NONE : CID = new CID("ID_NONE");
 
