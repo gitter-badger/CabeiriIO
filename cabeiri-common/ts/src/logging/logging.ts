06cce3ae773b29d9e849f072244dc51f7d05eb64
Vincent Picard <vincentpcrd@gmail.com>
2016-04-28 00:08:49 -0400

    Added logging utility

---
+++ cabeiri-common/ts/src/logging/logging.ts
@@ -0,0 +1,17 @@
+export class CabeiriLog
+{
+    public static warning(msg : string)
+    {
+        console.log(msg);
+    }
+
+    public static error(msg : string)
+    {
+        console.log(msg);
+    }
+
+    public static log(msg : string)
+    {
+        console.log(msg);
+    }
+} 
